c0bc7764b2c061fc69916a1abf8b51359f650e60
reemsarhan <115352161+reemsarhan@users.noreply.github.com>
2023-09-07 14:46:48 +0300

    Create SPI_Prog.c

---
+++ SlaveShome/MCAL/SPI/SPI_Prog.c
@@ -0,0 +1,59 @@
+/*
+ * SPI_Prog.c
+
+ *
+ *  Created on: Aug 24, 2023
+ *      Author: hp
+ */
+
+#include "../../LIB/BITMATH.h"
+#include "../../LIB/STD_TYPES.h"
+#include "../DIO/DIO.h"
+#include "SPI_Cfg.h"
+#include "SPI_Init.h"
+#include "SPI_Prv.h"
+
+void MSPI_voidMasterIntial()
+{
+	MDIO_voidSetPinDirection(PORTB,PIN5,OUTPUT);//mosi
+	MDIO_voidSetPinDirection(PORTB,PIN7,OUTPUT);//sck
+	MDIO_voidSetPinDirection(PORTB,PIN6,INPUT);//miso
+	MDIO_voidSetPinDirection(PORTB,PIN4,OUTPUT);
+
+	const u8 SPCR_Setting=0b01010001;
+	SPCR =SPCR_Setting;
+	MDIO_voidSetPinValue(PORTB,PIN4,HIGH);
+
+}
+
+
+
+void MSPI_voidMasterTranceive(u8 A_u8Data)
+{
+//	 static  u8 flag;
+	MDIO_voidSetPinValue(PORTB,PIN4,LOW);
+	SPDR=A_u8Data;
+
+	while (!(SPSR & (1<<SPIF)))	//wait till the transmitting is done
+		;
+
+
+}
+
+
+void MSPI_voidSlave_init()
+{
+	/* Set MISO output, all others input */
+	MDIO_voidSetPinDirection(PORTB,PIN6,OUTPUT);
+	/* Enable SPI */
+	SPCR = (1<<SPE);
+}
+
+u8 SPI_SlaveReceive(){
+	/* Wait for reception complete */
+	while(!(SPSR & (1<<SPIF)));
+	/* Return data register */
+	return SPDR;
+}
+
+
