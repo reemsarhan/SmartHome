93cab5fbcf5c20eda9f8b4b45b02337a48495936
reemsarhan <115352161+reemsarhan@users.noreply.github.com>
2023-09-07 14:34:29 +0300

    Create BITMATH.h

---
+++ SlaveShome/LIB/BITMATH.h
@@ -0,0 +1,17 @@
+/*
+ * BITMATH.h
+ *
+ */
+
+#ifndef LIB_BITMATH_H_
+#define LIB_BITMATH_H_
+
+#define SET_BIT(Reg, bitnum)       (Reg |=1<<bitnum)
+#define CLR_BIT(Reg, bitnum)       (Reg &=~(1<<bitnum))
+#define TOGGLE_BIT(Reg, bitnum)    (Reg ^=(1<<bitnum))
+#define GET_BIT(Reg, bitnum)       ((Reg>>bitnum)& 0x01)
+#define SET_BYTE(Reg,value)        ( Reg = value )
+#define IS_BIT_SET(reg,bit)  (reg&(1<<bit))>>bit
+#define IS_BIT_CLR(reg,bit)  !((reg&(1<<bit))>>bit)
+
+#endif /* LIB_BITMATH_H_ */
