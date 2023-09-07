0121641311c2f1bd47df1a192ec924a1b86039f5
reemsarhan <115352161+reemsarhan@users.noreply.github.com>
2023-09-07 14:41:44 +0300

    Create ADC_driver.h

---
+++ SlaveShome/MCAL/ADC/ADC_driver.h
@@ -0,0 +1,28 @@
+/*
+ * ADC_driver.h
+
+
+ */ 
+
+
+#ifndef ADC_DRIVER_H_
+#define ADC_DRIVER_H_
+#include "../../LIB/BITMATH.h"
+#include "../../LIB/STD_TYPES.h"
+/*
+	Function Name        : ADC_vinit
+	Function Returns     : void
+	Function Arguments   : void
+	Function Description : Initialize the ADC.
+*/
+void ADC_vinit(void);
+
+/*
+	Function Name        : ADC_u16Read
+	Function Returns     : uint16
+	Function Arguments   : void
+	Function Description : Read the value which converted by the ADC.
+*/
+u16 ADC_u16Read(void);
+
+#endif /* ADC_DRIVER_H_ */
