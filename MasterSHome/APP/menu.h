d83fafe3bf8e8157f75a2c706785f297d5bcc1aa
reemsarhan <115352161+reemsarhan@users.noreply.github.com>
2023-09-07 14:13:38 +0300

    Create menu.h

---
+++ MasterSHome/APP/menu.h
@@ -0,0 +1,17 @@
+/*
+ * menu.h
+ *
+ *  Created on: Sep 5, 2023
+ *      Author: Reem
+ */
+
+#ifndef APP_MENU_H_
+#define APP_MENU_H_
+
+
+
+void vMenuOption(const u8 u8SelectedRoom );
+//u8 u8GetKeyPressed();
+
+
+#endif /* APP_MENU_H_ */
