a7ff6830f4f6e3743861a924ec8696e4c1183741
reemsarhan <115352161+reemsarhan@users.noreply.github.com>
2023-09-07 14:16:48 +0300

    Create LCD_config.h

---
+++ HAL/LCD/LCD_config.h
@@ -0,0 +1,13 @@
+/*
+ * LCD_config.h
+ *
+ *  Created on: Sep 6, 2023
+ *      Author: Reem
+ */
+
+#ifndef HAL_LCD_LCD_CONFIG_H_
+#define HAL_LCD_LCD_CONFIG_H_
+
+#define eight_bits_mode
+
+#endif /* HAL_LCD_LCD_CONFIG_H_ */
